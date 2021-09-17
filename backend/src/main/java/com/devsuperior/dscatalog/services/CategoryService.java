a142aab062946239a42191b1679be08206a197c5
Douglas Vilas Boas <douglas_vilasboas@hotmail.com>
2021-09-16 21:28:59 -0300

    Find By Id

--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -1,6 +1,7 @@
 package com.devsuperior.dscatalog.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,4 +27,11 @@ public class CategoryService {
 		return listDTO;
 		
 		}
+
+	@Transactional(readOnly = true)
+	public CategoryDTO findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		Category entity = obj.get();
+		return new CategoryDTO(entity);
+	}
  }
